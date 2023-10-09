b77883b9773b4c27ce001cd4de218ae829d7f0fc
FFarinola89 <fabiofarinola27@gmail.com>
2023-10-09 14:41:08 +0200

    Aggiungere i file di progetto.

---
+++ WebApplication2/Controllers/ToolController.cs
@@ -0,0 +1,93 @@
+﻿using DevExpress.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class ToolController : Controller
+    {
+        private readonly Client _client = new Client();
+
+
+
+        public static GridViewSettings GetToolGridViewSettings()
+        {
+            var settings = new GridViewSettings
+            {
+                Width = Unit.Percentage(100),
+                Name = "ToolGrid"
+            };
+
+
+            settings.Columns.Add(x =>
+           {
+               x.FieldName = "IdTool";
+               x.Caption = "Id Tool";
+               x.ColumnType = MVCxGridViewColumnType.TextBox;
+               
+           });
+
+            settings.Columns.Add(x =>
+            {
+                x.FieldName = "BoschCode";
+                x.Caption = "Codice Bosch";
+                x.ColumnType = MVCxGridViewColumnType.TextBox;
+            });
+
+            settings.Columns.Add(x =>
+            {
+                x.FieldName = "Description";
+                x.Caption = "Descrizione";
+                x.ColumnType = MVCxGridViewColumnType.TextBox;
+            });
+
+            settings.Columns.Add(x =>
+            {
+                x.FieldName = "PrimarySupplier";
+                x.Caption = "Fornitore Primario";
+                x.ColumnType = MVCxGridViewColumnType.TextBox;
+            });
+
+            settings.Columns.Add(x =>
+            {
+                x.FieldName = "SecondarySupplier";
+                x.Caption = "Fornitore Secondario";
+                x.ColumnType = MVCxGridViewColumnType.TextBox;
+            });
+
+            settings.Columns.Add(x =>
+            {
+                x.FieldName = "Quantity";
+                x.Caption = "Quantità";
+                x.ColumnType = MVCxGridViewColumnType.TextBox;
+            });
+
+            settings.CallbackRouteValues = new { Controller = "Tool", Action = "RefreshToolGrid" };
+            return settings;
+
+
+        }
+
+
+        public async Task<ActionResult> RefreshToolGrid()
+        {
+            var tools = (await _client.GetAll()).ToList();
+
+            return PartialView("ToolGridView", tools);
+        }
+
+
+        public async Task<ActionResult> GetAll()
+        {
+            List<Tool> tools = (await _client.GetAll()).ToList();
+            return View(tools);
+        }
+
+    }  
+}
---
+++ WebApplication2/Models/Client.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Client
+    {
+        public async Task<List<Tool>> GetAll()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://10.0.0.59:51699/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                List<Tool> tools = new List<Tool>();
+                HttpResponseMessage response = await client.GetAsync("/api/tool");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    tools = JsonConvert.DeserializeObject<List<Tool>>(content);
+                }
+
+                return tools;
+            }
+        }
+    }
+}
